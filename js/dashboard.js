b4f69e5890ff099bd1c16fbccb2f04dfd67d1aaa
Oayao08 <ayaouomar@fpllefia.com>
2026-05-02 14:53:01 +0000

    Afegir lógica  de la app i css

---
+++ js/dashboard.js
@@ -0,0 +1,10 @@
+console.log("Funciona");
+//Lógica del temporizador
+
+let temporizador; // Guarda intervalo
+let tiempoRestante = 25 * 60; // 25 minutos en segundos
+let enMarcha = false; //funcionamiento
+
+// Selección elementos HTML
+
+const pantallaTiempo = document.getElementById("time-remaining');
